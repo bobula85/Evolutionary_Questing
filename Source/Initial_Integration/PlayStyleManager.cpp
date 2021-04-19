7d28d835efe00f31d0ea7adade79b6a5a89ae67d
bobula85 <72500415+bobula85@users.noreply.github.com>
2021-04-19 22:33:41 +0100

    Fitness calculation bug fixed
    completion multiplier was being used for both the complete and incomplete parts of the fitness calculation

--- Source/Initial_Integration/PlayStyleManager.cpp
+++ Source/Initial_Integration/PlayStyleManager.cpp
@@ -6,11 +6,10 @@
 // Sets default values
 void UPlayStyleManager::Init()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	//PrimaryActorTick.bCanEverTick = true;
-
-	m_dCompMultiplier = 1.2;
-	m_dCompMultiplier = 1.0;
+	// Multipliers used to change weighting of fitness function elements
+	// Could be exposed to the editor for easier modification
+	m_dCompMultiplier = 1.1;
+	m_dInCompMultiplier = 1.0;
 	m_dAbnMultiplier = 0.8;
 	m_dRejMultiplier = 1.3;
 
@@ -68,31 +67,31 @@ void UPlayStyleManager::CalculateFitness()
 {
 	// Calculate fitness using player preference statisics eg fitness = (((Completed*?) + Incomplete*?) - Abandoned *? - (Rejected*?))
 	// Set to 1 if zero for roulette wheel selection purposes
-	m_iKill_Fitness = (((m_iKill_Com * m_dCompMultiplier) + m_iKill_InCom * m_dCompMultiplier) - ((m_iKill_Abn * m_dAbnMultiplier) + (m_iKill_Rej*m_dRejMultiplier)));
+	m_iKill_Fitness = (((m_iKill_Com * m_dCompMultiplier) + (m_iKill_InCom * m_dInCompMultiplier)) - ((m_iKill_Abn * m_dAbnMultiplier) + (m_iKill_Rej*m_dRejMultiplier)));
 	if (m_iKill_Fitness <= 0)
 		m_iKill_Fitness = 1;
 
-	m_iGather_Fitness = (((m_iGather_Com * m_dCompMultiplier) + m_iGather_InCom * m_dCompMultiplier) - ((m_iGather_Abn * m_dAbnMultiplier) + (m_iGather_Rej * m_dRejMultiplier)));
+	m_iGather_Fitness = (((m_iGather_Com * m_dCompMultiplier) + (m_iGather_InCom * m_dInCompMultiplier)) - ((m_iGather_Abn * m_dAbnMultiplier) + (m_iGather_Rej * m_dRejMultiplier)));
 	if (m_iGather_Fitness <= 0)
 		m_iGather_Fitness = 1;
 
-	m_iFetch_Fitness = (((m_iFetch_Com * m_dCompMultiplier) + m_iFetch_InCom * m_dCompMultiplier) - ((m_iFetch_Abn * m_dAbnMultiplier) + (m_iFetch_Rej * m_dRejMultiplier)));
+	m_iFetch_Fitness = (((m_iFetch_Com * m_dCompMultiplier) + (m_iFetch_InCom * m_dInCompMultiplier)) - ((m_iFetch_Abn * m_dAbnMultiplier) + (m_iFetch_Rej * m_dRejMultiplier)));
 	if (m_iFetch_Fitness <= 0)
 		m_iFetch_Fitness = 1;
 
-	m_iExplore_Fitness = (((m_iExplore_Com * m_dCompMultiplier) + m_iExplore_InCom * m_dCompMultiplier) - ((m_iExplore_Abn * m_dAbnMultiplier) + (m_iExplore_Rej * m_dRejMultiplier)));
+	m_iExplore_Fitness = (((m_iExplore_Com * m_dCompMultiplier) + (m_iExplore_InCom * m_dInCompMultiplier)) - ((m_iExplore_Abn * m_dAbnMultiplier) + (m_iExplore_Rej * m_dRejMultiplier)));
 	if (m_iExplore_Fitness <= 0)
 		m_iExplore_Fitness = 1;
 	
-	m_iClose_Fitness = (((m_iClose_Com * m_dCompMultiplier) + m_iClose_InCom * m_dCompMultiplier) - ((m_iClose_Abn * m_dAbnMultiplier) + (m_iClose_Rej * m_dRejMultiplier)));
+	m_iClose_Fitness = (((m_iClose_Com * m_dCompMultiplier) + (m_iClose_InCom * m_dInCompMultiplier)) - ((m_iClose_Abn * m_dAbnMultiplier) + (m_iClose_Rej * m_dRejMultiplier)));
 	if (m_iClose_Fitness <= 0)
 		m_iClose_Fitness = 1;
 
-	m_iMid_Fitness = (((m_iMid_Com * m_dCompMultiplier) + m_iMid_InCom * m_dCompMultiplier) - ((m_iMid_Abn * m_dAbnMultiplier) + (m_iMid_Rej * m_dRejMultiplier)));
+	m_iMid_Fitness = (((m_iMid_Com * m_dCompMultiplier) + (m_iMid_InCom * m_dInCompMultiplier)) - ((m_iMid_Abn * m_dAbnMultiplier) + (m_iMid_Rej * m_dRejMultiplier)));
 	if (m_iMid_Fitness <= 0)
 		m_iMid_Fitness = 1;
 
-	m_iFar_Fitness = (((m_iFar_Com * m_dCompMultiplier) + m_iFar_InCom * m_dCompMultiplier) - ((m_iFar_Abn * m_dAbnMultiplier) + (m_iFar_Rej * m_dRejMultiplier)));
+	m_iFar_Fitness = (((m_iFar_Com * m_dCompMultiplier) + (m_iFar_InCom * m_dInCompMultiplier)) - ((m_iFar_Abn * m_dAbnMultiplier) + (m_iFar_Rej * m_dRejMultiplier)));
 	if (m_iFar_Fitness <= 0)
 		m_iFar_Fitness = 1;
 
